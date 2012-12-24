d525026ab9b82cfcffb387819bed9dacb8c2e202
Gordon Koo <gordon@gordonkoo.com>
2012-12-24 09:55:14 -0800

    remove nevercache

--- syte/views/home.py
+++ syte/views/home.py
@@ -7,14 +7,12 @@ from django.http import HttpResponseServerError
 from syte.context_processor import site_pages
 
 
-@never_cache
 def server_error(request, template_name='500.html'):
     t = loader.get_template(template_name)
     d = site_pages(request)
     return HttpResponseServerError(t.render(Context(d)))
 
 
-@never_cache
 def page_not_found_error(request, template_name='404.html'):
     t = loader.get_template(template_name)
     d = site_pages(request)
