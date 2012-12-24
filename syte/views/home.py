815d7f300f604aec5a620f439cb5626cf16dfefa
Gordon Koo <gordon@gordonkoo.com>
2012-12-24 09:59:54 -0800

    added nevercache back

--- syte/views/home.py
+++ syte/views/home.py
@@ -3,16 +3,19 @@ from django.conf import settings
 from django.shortcuts import render
 from django.template import Context, loader
 from django.http import HttpResponseServerError
+from django.views.decorators.cache import never_cache
 
 from syte.context_processor import site_pages
 
 
+@never_cache
 def server_error(request, template_name='500.html'):
     t = loader.get_template(template_name)
     d = site_pages(request)
     return HttpResponseServerError(t.render(Context(d)))
 
 
+@never_cache
 def page_not_found_error(request, template_name='404.html'):
     t = loader.get_template(template_name)
     d = site_pages(request)
